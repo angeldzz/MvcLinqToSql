affacf084bfb3568126d0946263f98f9e22ec184
angeldzz <pintodiazangel@gmail.com>
2026-01-29 14:14:42 +0100

    Add project files.

---
+++ MvcLinqToSql/Controllers/EmpleadosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcLinqToSql.Models;
+using MvcLinqToSql.Repositories;
+
+namespace MvcLinqToSql.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        RepositoryEmpleado repo;
+        public EmpleadosController()
+        {
+            this.repo = new RepositoryEmpleado();
+        }
+
+        public IActionResult Index()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+        public IActionResult Details(int idempleado)
+        {
+            Empleado empleado = this.repo.FindEmpleado(idempleado);
+            return View(empleado);
+        }
+    }
+}
---
+++ MvcLinqToSql/Repositories/RepositoryEmpleado.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+using MvcLinqToSql.Models;
+using System.Data;
+
+namespace MvcLinqToSql.Repositories
+{
+    public class RepositoryEmpleado
+    {
+        private DataTable tablaEmpleados;
+        public RepositoryEmpleado()
+        {
+            string connectionString = "Data Source=LOCALHOST\\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            string sql = "SELECT * FROM EMP";
+            //CREAMOS EL ADAPTADOR PUENTE ENTRE SQL SERVER Y LINQ
+            SqlDataAdapter ad = new SqlDataAdapter(sql, connectionString);
+            this.tablaEmpleados = new DataTable();
+            //TRAEMOS LOS DATOS PARA LINQ
+            ad.Fill(this.tablaEmpleados);
+        }
+        public List<Empleado> GetEmpleados()
+        {
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable()
+                           select datos;
+            //AHORA MISMO TENEMOS DENTRO DE LA CONSULTA LA INFORMACION DE LOS EMPELADOS
+            // DE LOS EMPLEADOS. LOS DATOS VIENEN EN FORMATO TABLA, CADA ELEMENTO
+            // ES UNA FILA LLAMADA DATAROW
+            //DEBEMOS RECORRER CADA FILA Y CONVERTIRLA A UN OBJETO EMPLEADO
+            List<Empleado> empleados = new List<Empleado>();
+            foreach (var row in consulta)
+            {
+                // para extraer los datos de un datarow usamos el nombre de la columna
+                //DataRow.Field<TIPODATO>("NOMBRECOLUMNA")
+                Empleado empleado = new Empleado();
+                empleado.IdEmpleado = row.Field<int>("EMP_NO");
+                empleado.Apellido = row.Field<string>("APELLIDO");
+                empleado.Oficio = row.Field<string>("OFICIO");
+                empleado.Salario = row.Field<int>("SALARIO");
+                empleado.IdDepartamento = row.Field<int>("DEPT_NO");
+                empleados.Add(empleado);
+            }
+            return empleados;
+        }
+        public Empleado FindEmpleado(int idEmpleado)
+        {
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable()
+                           where datos.Field<int>("EMP_NO") == idEmpleado
+                           select datos;
+            //Nosotros sabemos que esta consulta nos devuelve una sola fila
+            //pero linq nos devuelve un conjunto
+            //dentro de este conjunto tenemos metodos LAMBDA
+            //para hacer ciertas cosas
+            //por ejemplo, podriamos contar, podriamos saber el maximo, minimo, etc
+            var row = consulta.First();
+            Empleado empleado = new Empleado();
+            empleado.IdEmpleado = row.Field<int>("EMP_NO");
+            empleado.Apellido = row.Field<string>("APELLIDO");
+            empleado.Oficio = row.Field<string>("OFICIO");
+            empleado.Salario = row.Field<int>("SALARIO");
+            empleado.IdDepartamento = row.Field<int>("DEPT_NO");
+            return empleado;
+        }
+    }
+}
